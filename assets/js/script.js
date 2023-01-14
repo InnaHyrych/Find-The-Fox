7476370198db52778119cfe1ed51791e66fced2f
InnaHyrych <innainna3185@gmail.com>
2023-01-14 20:32:08 +0000

    Add function to select door buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,43 +6,64 @@ document.addEventListener("DOMContentLoaded", function() {
     questionsText.textContent = "Hey! Welcome to our little house)) The Fox hid somewhere in it. Can you find her? ... P.S. Dont go through the blue door!"; 
 })
 
-let buttonBlue = document.getElementById('blue');
-let buttonRed = document.getElementById('red');
-let buttonYellow = document.getElementById('yellow');
-let buttonGreen = document.getElementById('green');
 
-let buttonArray = [];
-    buttonArray.push(buttonBlue);
-    buttonArray.push(buttonRed);
-    buttonArray.push(buttonYellow);
-    buttonArray.push(buttonGreen);
+ let blueDoor = document.getElementById("blueanswer");        
 
-    console.log(buttonArray[2]);   
-
-
-     
-function random(event) {
-         buttonArray = buttonArray.sort(() => Math.random() - 0.5);
-    console.log(buttonArray[2]);           
-    
-}  
-
-//document.addEventListener("click", random);
-
- let blueDoor = document.getElementById("blueanswer");
-    console.log(blueDoor);      
-
-  function onBlueDoor() {
-        
+  function onBlueDoor() {        
             
             let Space = document.getElementById('space').style.display = 'block';
-            let House = document.getElementById('house').style.display = 'none';
-            console.log(buttonArray[0]);
             let Message1 = 'Hello! You are in space! There are no Foxes here, go back on the Earth!'  
             questionsText.textContent = Message1;
             console.log(Message1);           
             
-        }
-    
+        }    
 
     blueDoor.addEventListener("click", onBlueDoor);
+
+/*----------Create array for buttons*/
+
+    //let buttonBlue = document.getElementById('blue');
+let buttonRed = document.getElementById('redanswer');
+let buttonYellow = document.getElementById('yellowanswer');
+let buttonGreen = document.getElementById('greenanswer');
+
+let buttonArray = [];
+    //buttonArray.push(buttonBlue);
+    buttonArray.push(buttonRed);
+    buttonArray.push(buttonYellow);
+    buttonArray.push(buttonGreen);
+
+    console.log(buttonArray[2]);  
+    
+  
+for ( let button of buttonArray) {
+
+button.addEventListener("click", function() 
+    {
+        if (this.getAttribute('id') === 'redanswer') 
+        {
+            let Corridor = document.getElementById('corridor').style.display = 'block';
+            let Message2 = 'Corridor.'  
+            questionsText.textContent = Message2;
+            console.log(Message2); 
+        } 
+        else if (this.getAttribute('id') === 'yellowanswer') 
+        {
+            let Kitchen = document.getElementById('kitchen').style.display = 'block';
+            let Message3 = 'Kitchen.'  
+            questionsText.textContent = Message3;
+            console.log(Message3); 
+        } 
+        else 
+        {
+            let Bedroom = document.getElementById('bedroom').style.display = 'block';
+            let Message4 = 'Bedroom.'  
+            questionsText.textContent = Message4;
+            console.log(Message4);
+        }
+    
+    }
+    )
+} 
+
+
