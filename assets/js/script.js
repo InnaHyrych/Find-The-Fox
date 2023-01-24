d04bce00dd9b34e779a3c35e56eab63fa53ec41d
InnaHyrych <innainna3185@gmail.com>
2023-01-24 17:04:43 +0000

    Add favicon links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 var questionsText = document.getElementById('questions');
 function download() {
-    var house = document.getElementById('house');
-    house.style.display = 'block';
+    document.getElementById('house').style.display = 'block';
     questionsText.textContent = "Hey! Welcome to our little house)) The Fox hid somewhere in it. Can you find her? ... P.S. Don't go through the blue door!";
 }
 document.addEventListener("DOMContentLoaded", download); 
@@ -38,30 +37,45 @@ function clearScreen() {
     }
 }   
 
-let blueDoor = document.getElementById("blueanswer"); 
+let blueDoor = document.getElementById("blueanswer");
 
- 
+//function sound () {
+    
+//} 
 /**
 * When user push on the blue door, he always will see image with Spase
 * and page must be reload 
 */
 function onBlueDoor() {
     clearScreen();
-    let Space = document.getElementById('space').style.display = 'block';
-    let Message1 = 'You teleported to space! There are no Foxes here, cya ! .'; 
+    document.getElementById('space').style.display = 'block';
+    var Message1 = 'You teleported to space! There are no Foxes here, cya ! .'; 
     questionsText.textContent = Message1;
+    //sound();
     window.addEventListener('click', function() {
     //all buttons will disappear from the screen
     document.getElementById ('blueanswer').style.display = 'none';
     document.getElementById ('redanswer').style.display = 'none';
     document.getElementById ('yellowanswer').style.display = 'none';
     document.getElementById ('greenanswer').style.display = 'none';
-    let finishButton = document.getElementById('finish');
-    finishButton.style.display = "block";//Display button "Play Again"
+    document.getElementById('finish').style.display = "block";//Display button "Play Again"
+    
     });
-}            
+} 
 
 blueDoor.addEventListener("click", onBlueDoor);
+
+
+
+    //if (blueDoor.addEventListener("click", onBlueDoor)) {
+       // alert('Hi at first');
+        //var audio = new Audio();
+        //audio.src = ("assets/sound/wrong-place.mp3");
+        //audio.play();    
+    //}
+
+
+ 
 
 /*------------Create Array for functions*/
 let functionsArray = [
@@ -73,7 +87,7 @@ let functionsArray = [
  */
 function inCorridor() {
     clearScreen();
-    let Corridor = document.getElementById('corridor').style.display = 'block';
+    document.getElementById('corridor').style.display = 'block';
     let Message2 = `Foxes seem to be good at avoiding hard work - especially when there's someone else to do it for them. 
     Instead of bothering to dig their own den, they sometimes steal a badger's burrow (called a sett) by scaring the owner away 
     and making his house smell so bad he doesn't want it back.`;  
@@ -87,7 +101,7 @@ function inCorridor() {
  */
 function inKitchen() {
     clearScreen();
-    let Kitchen = document.getElementById('kitchen').style.display = 'block';
+    document.getElementById('kitchen').style.display = 'block';
     let Message3 = `Foxes are omnivores. This means that they eat meat and vegetation. 
     A fox's diet can consist of small animals, such as lizards, voles, rats, mice, rabbits and hares. They round out their diet with birds, fruits and bugs.`;
     questionsText.textContent = Message3;
@@ -100,7 +114,7 @@ function inKitchen() {
  */
 function inBedroom() {
     clearScreen();
-    let Bedroom = document.getElementById('bedroom').style.display = 'block';
+    document.getElementById('bedroom').style.display = 'block';
     let Message4 = `Foxes typically live in the forest and make their den by digging holes and making burrows for themselves.
      These will allow the fox to attain a cool area to sleep in. 
      Foxes also use their dens to store food to save for later and keep their pups safe from other animals.`;  
@@ -114,7 +128,7 @@ function inBedroom() {
  */
 function inDiningroom() {
     clearScreen();
-    let Diningroom = document.getElementById('diningroom').style.display = 'block';
+    document.getElementById('diningroom').style.display = 'block';
     let Message5 = ` Foxes seem to be very playful. They can often be seen playing with a fallen apple as though it were a ball, or chasing their own tails. 
     This fooling around sometimes serves a second purpose: if a curious rabbit comes closer to watch, it will find itself gobbled up by the hungry fox.`;  
     questionsText.textContent = Message5;
@@ -127,7 +141,7 @@ function inDiningroom() {
  */
 function inSitingroom() {
     clearScreen();
-    let Sitingroom = document.getElementById('sitingroom').style.display = 'block';
+    document.getElementById('sitingroom').style.display = 'block';
     let Message6 = `Scientists believe that foxes may have had relationships with humans before dogs did. 
     There are 16,500 year old remains of a man and his pet fox in Jordan, 
     which is 4000 years older than any dog and human remains we have found that have been buried together.`;  
@@ -142,27 +156,17 @@ function inSitingroom() {
  */
 function inFox() {
     clearScreen();
-    let Fox = document.getElementById('fox').style.display = 'block';
+    document.getElementById('fox').style.display = 'block';
     
     //Link
     
-    let a = document.createElement('a');    
-    let link = document.createTextNode("here");
-    a.appendChild(link);
-    a.title = "link";
-    a.id = "site";
-    a.setAttribute("href", "https://www.saveafox.org");
-    questionsText.appendChild(a);
-    let textLink = document.getElementById('site');
-
-    console.log(textLink);
-
-    questionsText.innerHTML = "Congratulation! You have found the Fox!" +  
-    "Foxes are beautiful animals, with their delicate faces and bushy tails, but don't just love them for their good looks." +  
-    "The fox species are intriguing animals, known for their intelligence, playfulness, and lithe athleticism." +  
-    "And you can help to save them! Go to " + a + "and learn how!";  
+    questionsText.innerHTML = `'Congratulation! You have found the Fox!  
+    Foxes are beautiful animals, with their delicate faces and bushy tails, but don't just love them for their good looks. 
+    The fox species are intriguing animals, known for their intelligence, playfulness, and lithe athleticism.  
+    And you can help to save them! Push <a href="https://www.saveafox.org" target="_blanc">here</a> and learn how!`;  
        
-    let finishButton = document.getElementById('finish').style.display = "block";//Display button "Play Again"
+    document.getElementById('finish').style.display = "block";//Display button "Play Again"
+    sound ();
 
     /**
      * Block all buttons ecsept "Play Again"
@@ -177,7 +181,7 @@ function inFox() {
  * Calls a random function from the "functionsArray" list, 
  * when the button was click
  */   
-for ( var button of buttonArray) {
+for (var button of buttonArray) {
     button.addEventListener('click', function() {       
         let img = functionsArray[Math.floor(Math.random() * functionsArray.length)];
         img();
