c110d251b781806e4bbfb5534602f37a5dca4d22
InnaHyrych <117390733+InnaHyrych@users.noreply.github.com>
2023-03-17 20:52:34 +0000

    Correct listener for blue button, add button for mute sound

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,111 +131,50 @@ function inRoom (roomImage, roomMessage) {
     roomImage.style.display = 'block';
     questionsText.textContent = roomMessage;
     imgList.splice(imgList[roomImage], 1);
-    messageArray.splice(messageArray[roomMessage], 1);    
+    messageArray.splice(messageArray[roomMessage], 1);
 }
-                 
-let blueDoor = document.getElementById("blueanswer");
 
-//function sound () {
-    
-//} 
+const finishButton = document.getElementById('finish');
+
+/**
+ * Reload the current URL after user pushed 'Play again' button
+ * to start game from the begining
+ */
+finishButton.addEventListener('click', function () {
+    document.location.reload();
+});
+
+const blueDoor = document.getElementById("blueanswer");
+
 /**
 * When user push on the blue door, he always will see image with Spase
 * and page must be reload 
 */
-function onBlueDoor() {
+blueDoor.addEventListener("click", function () {
     clearScreen();
     document.getElementById('space').style.display = 'block';
-    var Message1 = 'You teleported to space! There are no Foxes here, cya ! .'; 
-    questionsText.textContent = Message1;
-    
-    //Add sound after pushed blue button
-    if(document.getElementById('questions').textContent.includes('teleported'))
-    {
+    let message1 = 'You teleported to space! There are no Foxes here, cya !';
+    questionsText.textContent = message1;
+        
+    //Add sound after pushed blue button    
         let audio = new Audio();
         audio.src = ("assets/sound/wrong-place.mp3");
         audio.play();
-        
-    }
-    document.getElementById ('blueanswer').style.display = 'none';
-    document.getElementById ('redanswer').style.display = 'none';
-    document.getElementById ('yellowanswer').style.display = 'none';
-    document.getElementById ('greenanswer').style.display = 'none';
-    document.getElementById('finish').style.display = "block";//Display button "Play Again"
+        let stopSound = document.getElementById('stop');
+        stopSound.style.display = "block";
 
-} 
-blueDoor.addEventListener("click", onBlueDoor); 
-
-/*------------Create Array for functions*/
-let functionsArray = [
+        stopSound.addEventListener("click",function () {
+            audio.pause();
+            document.getElementById('stop').style.display = "block";
+        });                
     
-/**
- * Removes previous image,
- * makes image "corridor.jpg" visible When user push on the random button,
- * displey text under the image
- */
-function inCorridor() {
-    clearScreen();
-    document.getElementById('corridor').style.display = 'block';
-    let Message2 = `Foxes seem to be good at avoiding hard work - especially when there's someone else to do it for them. 
-    Instead of bothering to dig their own den, they sometimes steal a badger's burrow (called a sett) by scaring the owner away 
-    and making his house smell so bad he doesn't want it back.`;  
-    questionsText.textContent = Message2;
-},
-
-/**
- * Removes previous image,
- * makes image "kitchen.jpg" visible When user push on the random button,
- * displey text under the image
- */
-function inKitchen() {
-    clearScreen();
-    document.getElementById('kitchen').style.display = 'block';
-    let Message3 = `Foxes are omnivores. This means that they eat meat and vegetation. 
-    A fox's diet can consist of small animals, such as lizards, voles, rats, mice, rabbits and hares. They round out their diet with birds, fruits and bugs.`;
-    questionsText.textContent = Message3;
-},
-
-/**
- * Removes previous image,
- * makes image "bedroom.jpg" visible When user push on the random button,
- * displey text under the image
- */
-function inBedroom() {
-    clearScreen();
-    document.getElementById('bedroom').style.display = 'block';
-    let Message4 = `Foxes typically live in the forest and make their den by digging holes and making burrows for themselves.
-     These will allow the fox to attain a cool area to sleep in. 
-     Foxes also use their dens to store food to save for later and keep their pups safe from other animals.`;  
-    questionsText.textContent = Message4;
-},
-
-/**
- * Removes previous image,
- * makes image "diningroom.jpg" visible When user push on the random button,
- * displey text under the image
- */
-function inDiningroom() {
-    clearScreen();
-    document.getElementById('diningroom').style.display = 'block';
-    let Message5 = ` Foxes seem to be very playful. They can often be seen playing with a fallen apple as though it were a ball, or chasing their own tails. 
-    This fooling around sometimes serves a second purpose: if a curious rabbit comes closer to watch, it will find itself gobbled up by the hungry fox.`;  
-    questionsText.textContent = Message5;
-},
-
-/**
- * Removes previous image,
- * makes image "sitingroom.jpg" visible When user push on the random button,
- * displey text under the image
- */
-function inSitingroom() {
-    clearScreen();
-    document.getElementById('sitingroom').style.display = 'block';
-    let Message6 = `Scientists believe that foxes may have had relationships with humans before dogs did. 
-    There are 16,500 year old remains of a man and his pet fox in Jordan, 
-    which is 4000 years older than any dog and human remains we have found that have been buried together.`;  
-    questionsText.textContent = Message6;
-},
+    blueDoor.style.display = 'none';
+    buttonRed.style.display = 'none';
+    buttonYellow.style.display = 'none';
+    buttonGreen.style.display = 'none';
+    finishButton.style.display = 'block';    
+});
+                 
 
 /**
  * Removes previous image,
@@ -262,7 +201,7 @@ function inFox() {
     document.getElementById ('redanswer').disabled = true;
     document.getElementById ('yellowanswer').disabled = true;
     document.getElementById ('greenanswer').disabled = true;
-}]; 
+}; 
 
 /**
  * Calls a random function from the "functionsArray" list, 
@@ -277,12 +216,3 @@ for (var button of buttonArray) {
     });
 }          
     
-let finishButton = document.getElementById('finish');
-
-/**
- * Reload the current URL after user pushed 'Play again' button
- * to start game from the begining
- */
-finishButton.addEventListener('click', function () {
-    document.location.reload();
-    });
